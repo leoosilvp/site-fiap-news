a1e0c73d5d6a4b2b432e784d9164bf0d6f63c306
leoosilvp <leo965679@gmail.com>
2025-03-24 01:15:25 -0300

    primeiro commit1

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const likeButtons = document.querySelectorAll('.like-btn');
+
+    likeButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const likesCount = this.nextElementSibling;
+            let currentLikes = parseInt(likesCount.textContent);
+
+            if (this.classList.contains('liked')) {
+                currentLikes--;
+                this.classList.remove('liked');
+                this.textContent = '❤️';
+            } else {
+                currentLikes++;
+                this.classList.add('liked');
+                this.textContent = '💗';
+            }
+
+            likesCount.textContent = `${currentLikes} curtidas`;
+
+        });
+    });
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        gsap.to(window, { duration: 1.5, scrollTo: this.getAttribute("href") });
+    });
+});
+
